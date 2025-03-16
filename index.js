76a294de28f86885aca5ba9e3427f4cfc92b762d
Agnisha De <2025.cse09.agnishade.gmail.com>
2025-03-16 19:39:51 +0530

    Set up a Node.js server with routing for overview, product, and API endpoints. Implemented template rendering for product cards using HTML templates and integrated data from a JSON file i.e data.json

--- index.js
+++ index.js
@@ -33,14 +33,46 @@ const url = require('url');
 /////////////////////////////////
 // SERVER
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+function replaceTemplate(temp, product) {   //temp is the template and product is the object  
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  
+  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+}
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObj = JSON.parse(data);
+const dataObj = JSON.parse(data);    //dataObj holds all of the products details in the form of an array of objects
 
 const server = http.createServer((req, res) => {
     const pathname = req.url;
     if (pathname === '/' || pathname === '/overview') {
-        res.end('This is the OVERVIEW');
-      }
+        res.writeHead(200, {'Content-type': 'text/html'});
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+
+        res.end(output);
+    }
 
     else if (pathname === '/product') {
         res.end('This is the PRODUCT');}
